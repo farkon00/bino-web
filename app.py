4d98fb6317f918b503206e6e7434ebd10e70fced
farkon00 <sammer2016sammer@gmail.com>
2022-08-11 22:42:49 +0300

    Added template

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.debug = True
 
 @app.route("/")
 def home():
-    return "Hello World!"
+    return render_template("index.html")
