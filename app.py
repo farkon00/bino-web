a0d536461a4eaaa3a75d3c0a98f2c2741bcefcd3
farkon00 <sammer2016sammer@gmail.com>
2022-08-15 17:50:05 +0300

    Fixed bugs

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ from std_wrapper import *
 
 DEFAULT_TIME_LIMIT = 7
 
+out = sys.stdout
+
 app = Flask(__name__)
 app.debug = True
 
@@ -22,16 +24,16 @@ def home():
 
 codes = {}
 
-def time_limiter(sid, proc : multiprocessing.Process, emit_queue):
+def time_limiter(sid, proc : multiprocessing.Process, pipe):
     if proc.is_alive():
         proc.kill()
         while proc.is_alive():
             pass
         proc.close()
-        emit_queue.append(("reset_out", sid))
-        emit_queue.append(("add_out", f"Your program was running over {DEFAULT_TIME_LIMIT} seconds.\nYou can run this program using binarian locally installed on your pc.", sid))
 
-def run_inter(sid, code, emit_queue):
+        pipe.send(f"Your program was running over {DEFAULT_TIME_LIMIT} seconds.\nYou can run this program using binarian locally installed on your pc.")
+
+def run_inter(code, pipe):
     out = OutputWrapper()
     sys.stdout = out
     sys.stderr = out
@@ -43,14 +45,15 @@ def run_inter(sid, code, emit_queue):
         print_exc()
     except BaseException:
         pass
-    emit_queue.append(("reset_out", sid))
-    emit_queue.append(("add_out", out.output, sid))
+    pipe.send(out.output)
 
 def run_code(code, sid):
     emit_queue = []
 
-    interp = multiprocessing.Process(target=run_inter, args=(sid, code, emit_queue))
-    limiter = threading.Timer(DEFAULT_TIME_LIMIT, time_limiter, (sid, interp, emit_queue))
+    pipe_rec, pipe_sen = multiprocessing.Pipe(False)
+
+    interp = multiprocessing.Process(target=run_inter, args=(code, pipe_sen))
+    limiter = threading.Timer(DEFAULT_TIME_LIMIT, time_limiter, (sid, interp, pipe_sen))
 
     interp.start()
     limiter.start()
@@ -59,6 +62,16 @@ def run_code(code, sid):
         if emit_queue:
             emit_request = emit_queue.pop(0)
             emit(*emit_request[:-1], to=emit_request[-1])
+        try:
+            is_alive = interp.is_alive()
+        except ValueError:
+            is_alive = False
+        if not is_alive:
+            rec = pipe_rec.recv()
+            print(rec, file=out)
+            emit("reset_out", to=sid)
+            emit("add_out", rec, to=sid)
+            break
 
 @socketio.on("connect")
 def connect():
