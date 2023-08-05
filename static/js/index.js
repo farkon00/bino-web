77477942058e25dc68653ad424af7c90ebdfa12b
farkon00 <sammer2016sammer@gmail.com>
2023-08-06 00:48:16 +0300

    Added examples

--- static/js/index.js
+++ static/js/index.js
@@ -27,3 +27,72 @@ function sendCode() {
     clearTimeout(send_timer);
     send_timer = setTimeout(sendCode, send_timer_time);
 }
+
+const dropdown_options = document.getElementsByClassName("dropdown-option");
+for (let i = 0; i < dropdown_options.length; i++) {
+    dropdown_options[i].addEventListener("click", function() {
+        code_area.value = examples[dropdown_options[i].getAttribute("data-example-id")];
+        document.getElementById("dropdown-text").innerText = dropdown_options[i].innerText;
+    });
+}
+
+const examples = {
+    hello_world: 'print "Hello, world!"',
+    fibonacci: 
+`func list fib : n: int {
+    if (== n 0) {
+        return []
+    }
+    if (== n 1) {
+        return [1]
+    }
+
+    var res = [1 1]
+    while (< (len res) n) {
+        append res (+ (index res -1) (index res -2))
+    }
+    return res
+}
+
+print (fib 8)
+`,
+    rule110:
+`// Input
+var initial = [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1]
+var iterations = 16
+
+var size = (len initial)
+var states = [initial]
+
+var last-state = initial
+var i = 0
+while (< i (- iterations 1)) {
+    var j = 0
+    var new-state = []
+    while (< j size) {
+        var p = (index last-state (- j 1))
+        var q = (index last-state j)
+        var r = (index last-state (% (+ j 1) size))
+        append new-state (or (and q (not p)) (xor q r))
+        var j = (+ j 1)
+    }
+    var last-state = new-state
+    append states last-state
+
+    var i = (+ i 1)
+}
+
+var buffer = "" 
+for i states {
+    for j i {
+        if j {
+            var buffer = (+ buffer "*")
+        }
+        else {
+            var buffer = (+ buffer " ")
+        }
+    }
+    var buffer = (+ buffer "\\n")
+}
+print buffer`
+};
